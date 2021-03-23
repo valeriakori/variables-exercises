d172ba6bdd5a086f707a44a28c49487c3f04f683
Anna Val Kor <valeria@devhausleipzig.de>
2021-03-23 15:29:30 +0000

    add new excercise

--- 06-mad-horoscope/src/index.ts
+++ 06-mad-horoscope/src/index.ts
@@ -1,3 +1,12 @@
 /*
-*
+* Use Template literals to wrap the variables
+* noun, adjective and pluralNoun.
+* Assign values to the variables and log the statement.
 */
+
+const noun = ;
+const adjective = ;
+const pluralNoun = ;
+
+const fortune = "Those born under the planetary sign of the noun possess adjective personalities and are forever searching for new pluralNoun to conquer.";
+
