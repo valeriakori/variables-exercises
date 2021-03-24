4c487d4f2de12e919a294c6be48bb049936c365f
Anna Val Kor <valeria@devhausleipzig.de>
2021-03-24 14:55:15 +0000

    add exercise

---
+++ 09-input-to-object/src/index.ts
@@ -0,0 +1,7 @@
+/*
+* Write a programm that expects 2 commandline parameters:
+* name and favorite food.
+*
+* Store this information in an object
+* Print the sentence "[Name] likes [favorite food] the most!"
+*/
